b2c68156ce3432581857947c07bbe4fdf1c00590
Alexandr Gorbishev <alexandrgorbishev@inbox.ru>
2021-04-21 17:49:53 +0300

    Task 2

--- HW.py
+++ HW.py
@@ -6,6 +6,14 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+    def rate_lecturer(self, lecturer, grade):
+        if isinstance(lecturer, Lecturer) and (set(self.courses_in_progress) & set(lecturer.courses_attached)):
+            if isinstance(grade, int) and (0 <= grade <= 10) :
+                lecturer.grades += [grade]
+            else:
+                print("Оценка введена неверно")
+        else:
+            return 'Ошибка'
 
 class Mentor:
     def __init__(self, name, surname):
@@ -13,6 +21,12 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = []
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -22,24 +36,27 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    pass
-class Reviewer(Mentor):
-    pass
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
 Lecturer_1 = Lecturer('Leps', 'Grisha')
 Lecturer_1.courses_attached += ['Python']
 Lecturer_1.courses_attached += ['Java']
+
+Lecturer_2 = Lecturer('Jigurda', 'Nikita')
+Lecturer_2.courses_attached += ['JS']
+
 Reviewer_1 = Reviewer('Nikolaev', 'Igor')
 Reviewer_1.courses_attached += ['Python']
 
-
+Reviewer_1.rate_hw(best_student, 'Python', 8)
+Reviewer_1.rate_hw(best_student, 'Python', 9)
 Reviewer_1.rate_hw(best_student, 'Python', 10)
-Reviewer_1.rate_hw(best_student, 'Python', 10)
-Lecturer_1.rate_hw(best_student, 'Python', 10)
 
-print(best_student.grades)
-print(Reviewer_1.__dict__)
+best_student.rate_lecturer(Lecturer_1, 10)
+best_student.rate_lecturer(Lecturer_1, 9)
+best_student.rate_lecturer(Lecturer_2, 7)
+print("student:", best_student.grades)
+#print("rev1:", Reviewer_1.__dict__)
+print("lec1:", Lecturer_1.grades)
+print("lec2:", Lecturer_2.grades)
