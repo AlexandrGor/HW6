5d25bf1564a5e8faadab6936c5b11a11617bdf52
Alexandr Gorbishev <alexandrgorbishev@inbox.ru>
2021-04-21 16:40:37 +0300

    Task 1

--- HW.py
+++ HW.py
@@ -22,14 +22,24 @@ class Mentor:
         else:
             return 'Ошибка'
 
+class Lecturer(Mentor):
+    pass
+class Reviewer(Mentor):
+    pass
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+Lecturer_1 = Lecturer('Leps', 'Grisha')
+Lecturer_1.courses_attached += ['Python']
+Lecturer_1.courses_attached += ['Java']
+Reviewer_1 = Reviewer('Nikolaev', 'Igor')
+Reviewer_1.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+
+Reviewer_1.rate_hw(best_student, 'Python', 10)
+Reviewer_1.rate_hw(best_student, 'Python', 10)
+Lecturer_1.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
+print(Reviewer_1.__dict__)
